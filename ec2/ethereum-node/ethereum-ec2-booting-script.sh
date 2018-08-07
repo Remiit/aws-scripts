4f7d99fda1ce446b141e766d624c4a4ed9d88dcf
sw-yoonsung <sw.yoonsung@gmail.com>
2018-08-07 10:30:15 +0900

    make install scripts get the script files from the github.

--- ec2/ethereum-node/ethereum-ec2-booting-script.sh
+++ ec2/ethereum-node/ethereum-ec2-booting-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir scripts
-sudo curl -o 
-sudo curl -o 
+sudo curl -o scripts/ethereum-install-ubuntu.sh https://raw.githubusercontent.com/Remiit/aws-scripts/master/ec2/ethereum-node/ethereum-install-ubuntu.sh
+sudo curl -o scripts/geth-sync.sh https://raw.githubusercontent.com/Remiit/aws-scripts/master/ec2/ethereum-node/geth-sync.sh 
 sudo chmod -R +x,+x,+x scripts
 sudo scripts/ethereum-install-ubuntu.sh
