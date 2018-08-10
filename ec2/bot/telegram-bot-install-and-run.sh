d86ea699ea64c987d944a677a7dd9e2fd147411b
sw-yoonsung <sw.yoonsung@gmail.com>
2018-08-10 17:48:57 +0900

    modify bot shell script

--- ec2/bot/telegram-bot-install-and-run.sh
+++ ec2/bot/telegram-bot-install-and-run.sh
@@ -4,9 +4,10 @@ HOME_PATH=$HOME
 PROJECT_PATH="$HOME_PATH/remiit-telegram-bot-node"
 CONFIG_PATH="$PROJECT_PATH/config"
 S3_TOKEN_PATH="s3://bluepan-dev/telegram-bot/token.json"
+NPM_PATH="$HOME/.nvm/versions/node/v8.11.3/bin"
 
 cmd_download_source="git clone $GIT_REPO $PROJECT_PATH"
-cmd_download_token_from_s3="aws s3 $S3_TOKEN_PATH $CONFIG_PATH/"
+cmd_download_token_from_s3="aws s3 cp $S3_TOKEN_PATH $CONFIG_PATH/"
 
 eval "mkdir $PROJECT_PATH"
 eval $cmd_download_source 
@@ -14,5 +15,5 @@ eval $cmd_download_token_from_s3
 
 PRE_FIX="--prefix $PROJECT_PATH"
 cd $PROJECT_PATH
-eval $("npm install 2>&1 | tee install_log.txt")
-eval $("npm start") 
+eval $("$NPM_PATH/npm install 2>&1 | tee install_log.txt")
+eval $("$NPM_PATH/npm start") 
