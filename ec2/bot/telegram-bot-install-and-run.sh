a533dc3fe4005aae5565466d5c6ac6bf8443cf12
sw-yoonsung <sw.yoonsung@gmail.com>
2018-08-10 17:42:58 +0900

    modify bot shell script.

--- ec2/bot/telegram-bot-install-and-run.sh
+++ ec2/bot/telegram-bot-install-and-run.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 GIT_REPO="https://github.com/RemiitAdmin/remiit-telegram-bot-node.git"
-PATH="~/"
-PROJECT_PATH="$PATH/remiit-telegram-bot-node"
-CONFIG_PATH=$PROJECT_PATH/config
-S3_TOKEN_PATH=s3://bluepan-dev/telegram-bot/token.json
-eval $(cd $PATH)
+HOME_PATH=$HOME
+PROJECT_PATH="$HOME_PATH/remiit-telegram-bot-node"
+CONFIG_PATH="$PROJECT_PATH/config"
+S3_TOKEN_PATH="s3://bluepan-dev/telegram-bot/token.json"
 
-cmd_download_source=$(git clone $GIT_REPO)
-cmd_download_token_from_s3=$(aws s3 $S3_TOKEN_PATH $CONFIG_PATH/)
+cmd_download_source="git clone $GIT_REPO $PROJECT_PATH"
+cmd_download_token_from_s3="aws s3 $S3_TOKEN_PATH $CONFIG_PATH/"
 
+eval "mkdir $PROJECT_PATH"
 eval $cmd_download_source 
 eval $cmd_download_token_from_s3
 
-eval $(cd $PROJECT_PATH)
-
-eval $(npm install 2>&1 | tee install_log.txt)
-eval $(npm start)
+PRE_FIX="--prefix $PROJECT_PATH"
+cd $PROJECT_PATH
+eval $("npm install 2>&1 | tee install_log.txt")
+eval $("npm start") 
